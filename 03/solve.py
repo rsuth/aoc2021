1ec853fe854ef3c50f2c4822573b82e919e7a26e
Rick Sutherland <suthe.rick@gmail.com>
2021-12-03 14:12:38 -0800

    day3

---
+++ 03/solve.py
@@ -0,0 +1,45 @@
+with open('./input.txt', 'r') as f:
+    numbers = []
+    for l in f:
+        numbers.append(l.strip())
+
+def get_most_common(column, numbers, inv=False):
+  ones = 0
+  zeros = 0
+  for n in numbers:
+    if n[column] == "1":
+      ones += 1
+    else:
+      zeros += 1
+  if inv:
+    return '0' if ones >= zeros else '1'
+  else:
+    return '1' if ones >= zeros else '0'
+
+def find_gamma(numbers):
+  cols = len(numbers[0])
+  ret = ""
+  for col in range(cols):
+    ret += get_most_common(col, numbers)
+  return ret
+
+def find_epsilon(gamma):
+  return ''.join('1' if x =='0' else '0' for x in gamma)
+
+print(f"part 1: {int(find_gamma(numbers),2) * int(find_epsilon(find_gamma(numbers)), 2)}")
+
+def o2(numbers, col):
+  if len(numbers) == 1:
+    return numbers
+  else:
+    filtered = list(filter(lambda num: num[col] == get_most_common(col, numbers), numbers))
+    return o2(filtered, col+1)
+
+def co2(numbers, col):
+  if len(numbers) == 1:
+    return numbers
+  else:
+    filtered = list(filter(lambda num: num[col] == get_most_common(col, numbers, True), numbers))
+    return co2(filtered, col+1)
+
+print(f"part 2: {int(o2(numbers, 0)[0], 2) * int(co2(numbers, 0)[0], 2)}")
